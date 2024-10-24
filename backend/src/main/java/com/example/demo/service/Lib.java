02d902aa058f349168ab8645291fdf5ecdcc5407
tiennguyenvan <nguyentien.jobs@gmail.com>
2024-10-23 17:46:55 -0700

    Frame for ValidationCode

---
+++ backend/src/main/java/com/example/demo/service/Lib.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+public class Lib {
+
+	public static String ValidationCode() {	
+		// we will use this for testing purposes
+		return "123456";
+	}
+	public static String ValidationCode(int length) {
+		// please develop this with a real implementation
+		return "123456";
+	}
+}
