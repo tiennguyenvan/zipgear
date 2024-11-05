ac49acbd7b75c990c033536a0862bde69ce2332e
Ed-chae <158526143+Ed-chae@users.noreply.github.com>
2024-11-04 22:12:32 -0800

    Update router.js

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -4,11 +4,18 @@ import ProfilePage from '@/views/ProfilePage.vue';
 import HomePage from '@/views/HomePage.vue';
 import CheckoutPage from '@/views/CheckoutPage.vue';
 
+import ProductListPage from '@/views/ProductListPage.vue'; // Product list route
+import ProductFormPage from '@/views/ProductFormPage.vue'; // Product form route
+
 const routes = [
 	{ path: '/', name: 'home', component: HomePage },
 	{ path: '/login', name: 'login', component: LoginPage },
 	{ path: '/profile', name: 'profile', component: ProfilePage }, // Profile route
-	{ path: '/checkout', name: 'checkout', component: CheckoutPage }, // Profile route
+	{ path: '/checkout', name: 'checkout', component: CheckoutPage }, // Checkout route
+
+	{ path: '/products', name: 'productList', component: ProductListPage }, // Product list route
+	{ path: '/products/new', name: 'productCreate', component: ProductFormPage }, // Create product route
+	{ path: '/products/edit/:id', name: 'productEdit', component: ProductFormPage, props: true }, // Edit product route
 ];
 
 const router = createRouter({
