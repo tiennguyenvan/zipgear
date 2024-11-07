642a63e405ca308b83e169a29a6eb89302597911
tiennguyenvan <nguyentien.jobs@gmail.com>
2024-11-06 23:08:02 -0800

    Add boilerplate Pages

---
+++ frontend/src/views/ProductDetailPage.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="wide">
+		<NavBar />
+
+		<div class="card user-profile flex wrapper">
+			<div class="product-detail main">
+				<h2>Place Product Image here</h2>							
+			</div>
+			
+			<div class="info sidebar">
+				<h2>Place Produc INFORMATION here</h2>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, pariatur quasi. Eligendi pariatur atque corrupti, quis, totam architecto magnam inventore in enim consectetur voluptas error! Reprehenderit aperiam officia itaque officiis.
+				</p>
+			</div>
+		</div>
+	</div>
+</template>
