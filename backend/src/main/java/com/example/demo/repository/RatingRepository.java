b5d0e89a1831687eb3bcdce2ad0d14a31ab9f043
Ed-chae <158526143+Ed-chae@users.noreply.github.com>
2024-11-09 00:09:20 -0800

    Update RatingRepository.java

--- backend/src/main/java/com/example/demo/repository/RatingRepository.java
+++ backend/src/main/java/com/example/demo/repository/RatingRepository.java
@@ -1,6 +1,6 @@
 package com.example.demo.repository;
 
-import com.zipgear.entity.Rating;
+import com.example.demo.model.Rating;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
