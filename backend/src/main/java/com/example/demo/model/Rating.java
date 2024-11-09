ad7f22feee1c1e750f6ddf3221ea011f6676ced1
Ed-chae <158526143+Ed-chae@users.noreply.github.com>
2024-11-09 00:14:14 -0800

    Update Rating.java

--- backend/src/main/java/com/example/demo/model/Rating.java
+++ backend/src/main/java/com/example/demo/model/Rating.java
@@ -1,7 +1,16 @@
 package com.example.demo.model;
 
-import javax.persistence.*;
+
 import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "ratings")
