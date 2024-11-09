527d303ea01078dfbb893230e8cca8b13dc0bf73
Ed-chae <158526143+Ed-chae@users.noreply.github.com>
2024-11-09 00:01:43 -0800

    Update RatingController.java

--- backend/src/main/java/com/example/demo/controller/RatingController.java
+++ backend/src/main/java/com/example/demo/controller/RatingController.java
@@ -1,7 +1,7 @@
 package com.example.demo.controller;
 
-import com.zipgear.entity.Rating;
-import com.zipgear.repository.RatingRepository;
+import com.example.demo.model.Rating;
+import com.example.demo.repository.RatingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
